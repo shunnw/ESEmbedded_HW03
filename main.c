b841acee5e27b5f302c97f6fa151dd6dde098fb0
shunnw <N96064094@mail.ncku.edu.tw>
2019-03-19 12:58:18 +0800

    Revise main.c

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
+int addtwo(int a, int b)
+{
+	return a+b;
+}
+
+int countfive(int a, int b, int c, int d, int e)
+{
+	return a+(b*c)+d-e;
+}
+
 void reset_handler(void)
 {
+	addtwo(1,2);
+	countfive(1,2,3,4,5);
 	while (1)
 		;
 }
